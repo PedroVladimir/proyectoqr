e6d4edd82d769203aa779e3b5e49a49068b062b7
PedroVladimir <pvfloresrios@gmail.com>
2025-07-13 19:51:35 -0400

    update lista qr

--- app_qr/validator/validators.py
+++ app_qr/validator/validators.py
@@ -1,9 +1,9 @@
 from django.core.exceptions import ValidationError
 
 def validate_texto_length(value):
-    if not (4 <= len(value) <= 40):
+    if not (4 <= len(value) <= 50):
         raise ValidationError('El texto debe tener entre 4 y 40 caracteres.')
 
 def validate_descripcion_length(value):
-    if not (4 <= len(value) <= 50):
+    if not (4 <= len(value) <= 70):
         raise ValidationError('La descripción debe tener entre 4 y 50 caracteres.')
